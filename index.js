085c1373f8e66f479527109d84d889830a57b652
sreekanthkotagiri <sreekanthk@celcomsolutions.com>
2022-06-11 23:39:34 +0530

    adas

--- index.js
+++ index.js
@@ -7,29 +7,7 @@ const bodyParser = require('body-parser'); // Middleware ();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-  const client = new Client({
-  user: "yrntvcdjivsjqb",
-  host: "ec2-52-21-111-28.compute-1.amazonaws.com",
-  database: "dd4ll91o52gqhb",
-  password: "f27a2fed94bafd41321e92fe1933cd36bac409463456c9badff1c620e53d4465",
-  port: 5432,
-  ssl: true
-});
-
-client.connect();
-
-client.query('Select now()',(err, res)=>
-{
-  if (!err) {
-    console.log(res.rows);
-  }
-  else {
-    console.log(err.message)
-  }
-})
-
-
-app.listen(3000, () => {
+ app.listen(3000, () => {
   console.log("Application started and Listening on port 3000");
 });
 
